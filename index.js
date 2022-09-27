42fd59654a216d35e960bb1f05d32b7cfba190fb
Matthew Zenn <spyguy57@gmail.com>
2022-09-27 12:00:30 -0400

    Made covers for electronic and classical

---
+++ index.js
@@ -0,0 +1,110 @@
+const button = document.getElementById("button");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const genre = document.getElementById("genre");
+
+const matrix = [];
+matrix.length = 50;
+
+
+button.addEventListener("click", function() {
+    if (genre.value == "Electronic") {
+        Stripes();
+    }
+
+    else if (genre.value == "Classical/Ambient") {
+        Plaid();
+    }
+});
+
+function Stripes() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    matrix.fill(0);
+
+    for(var i = 0; i < matrix.length; i++) {
+        var x = Math.random()
+        if (x < .5) {
+        matrix[i] = 1
+        }
+    }
+
+    for(var i = 0; i < matrix.length; i++) {
+        var rectData = {
+            "x": i,
+            "y": 0,
+            "width": 1,
+            "height": 50
+        };
+
+        var huel = Math.floor(Math.random() * (360));
+
+        canvas.style.backgroundColor = 'black';
+        ctx.fillStyle = `hsl(${huel}, ${100}%, 50%)`;
+
+        if (matrix[i] == 1) {
+        ctx.beginPath();
+        ctx.fillRect(rectData.x,rectData.y, rectData.width, rectData.height);
+        ctx.closePath();
+       }
+    }
+
+    console.log(matrix);
+}
+
+function Plaid() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    matrix.fill(0);
+
+    for(var i = 0; i < matrix.length; i++) {
+        var x = Math.random()
+        if (x < .2) {
+        matrix[i] = 1
+        }
+    }
+
+    for(var i = 0; i < matrix.length; i++) {
+        var rectData = {
+            "x": i,
+            "y": 0,
+            "width": 1,
+            "height": 50
+        };
+
+        canvas.style.backgroundColor = 'wheat';
+        ctx.fillStyle = 'rgb(143, 56, 56)';
+
+        if (matrix[i] == 1) {
+        ctx.beginPath();
+        ctx.fillRect(rectData.x,rectData.y, rectData.width, rectData.height);
+        ctx.closePath();
+       }
+    }
+
+    matrix.fill(0);
+
+    for(var i = 0; i < matrix.length; i++) {
+        var x = Math.random()
+        if (x < .2) {
+        matrix[i] = 1
+        }
+    }
+
+    for(var i = 0; i < matrix.length; i++) {
+        var rectData = {
+            "x": 0,
+            "y": i,
+            "width": 50,
+            "height": 1
+        };
+
+        ctx.fillStyle = 'rgb(32, 47, 78)';
+
+        if (matrix[i] == 1) {
+        ctx.beginPath();
+        ctx.fillRect(rectData.x,rectData.y, rectData.width, rectData.height);
+        ctx.closePath();
+       }
+    }
+
+    console.log(matrix);
+}
